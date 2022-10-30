307d269f4c8e18c16af2cfbb6aec000b30dd5544
xitie2000 <1302928764@qq.com>
2022-10-30 04:29:39 +0000

    update

--- docs/.vuepress/sidebar.ts
+++ docs/.vuepress/sidebar.ts
@@ -16,4 +16,9 @@ export default [
         collapsible: true,
         children: ['/高级分布式系统/1-绪论', '/高级分布式系统/2-进程线程及客户服务器'],
     },
+    {
+        text: '占位页',
+        collapsible: true,
+        children: ['/占位页/占位1','/占位页/占位2'],
+    }
 ] as SidebarConfig;
