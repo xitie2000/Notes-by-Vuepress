e34053bb50acf5456bef3d33598e97597119fdd8
xitie2000 <1302928764@qq.com>
2023-04-06 07:35:19 +0000

    remove oss

--- docs/.vuepress/layouts/Layout.vue
+++ docs/.vuepress/layouts/Layout.vue
@@ -3,18 +3,18 @@ import ParentLayout from '@vuepress/theme-default/layouts/Layout.vue'
 </script>
 
 <template>
-<link rel="stylesheet" href="https://shuocloud-db.oss-cn-hangzhou.aliyuncs.com/music/APlayer.min.css">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/xitie2000/BlogSource@latest/js/APlayer.min.css">
       <div id="aplayer"></div>
-      <component :is="'script'" type="text/javascript" src="https://shuocloud-db.oss-cn-hangzhou.aliyuncs.com/music/APlayer.min.js"></component>
+      <component :is="'script'" type="text/javascript" src="https://cdn.jsdelivr.net/gh/xitie2000/BlogSource@latest/js/APlayer.min.js"></component>
 <div id="sakana-box" style="
     position: fixed;
     right: 0;
     bottom: 0;
     transform-origin: 100% 100%;
 "></div>
-        <component :is="'script'" type="text/javascript" src="https://shuocloud-db.oss-cn-hangzhou.aliyuncs.com/music/sakana.js"></component>
+        <component :is="'script'" type="text/javascript" src="https://cdn.jsdelivr.net/gh/xitie2000/BlogSource@latest/js/sakana.js"></component>
 
-        <component :is="'script'" type="text/javascript" src="https://shuocloud-db.oss-cn-hangzhou.aliyuncs.com/music/musicp.js"></component>
+        <component :is="'script'" type="text/javascript" src="https://cdn.jsdelivr.net/gh/xitie2000/BlogSource@latest/js/musicp.js"></component>
         <component :is="'script'"
         async
         onload="initSakanaWidget()"
